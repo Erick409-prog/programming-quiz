7b16cb1db83f6352488d3fb24b5d333897d77f53
Erick409-prog <erickjr0816@gmail.com>
2025-11-30 13:14:40 -0800

    Correct syntax and enhance MadLibs script
    Fixed syntax errors and improved formatting in the MadLibs script.

--- madLibsQuiz1-1.py
+++ madLibsQuiz1-1.py
@@ -1,6 +1,6 @@
 #This is a MadLibs project 
-#My name: 
-#Who I collaborated with: 
+#My name: Erick Mendoza
+#Who I collaborated with: No one 
 
 print("************************************")
 print("|                                  |")
@@ -9,7 +9,7 @@ print("|                                  |")
 print(************************************)
 
 (''MadLibs is a fill-in-the-blanks story
-game. The player must choose words based on the 
+game. the player must choose words based on the 
 given prompts, and the computer will return a 
 short story that includes the words the user chose.''')
 
@@ -19,7 +19,7 @@ if play = "y"
     person_name = input("Choose a name for a person: ")
     place = input("Choose a place: ")
     noun_1 = input("Choose a singlular noun: ")
-    animal_1 = input(Choose an animal: )
+    animal_1 = input("Choose an animal:" )
     adjective_1 = input("Choose an adjective for a feeling: ")
     adjective_2 = input("Choose an adjective: ")
     adjective_3  input("Choose an adjective: ")
@@ -27,13 +27,13 @@ if play = "y"
     food = input("Choose a food: ")
     
     print("""
-    Over break I am going camping with """ +person_name+""". It is important 
-    to be prepared when camping at place , so I made sure to pack a 
+    Over break, I am going camping with """ +person_name+""". It is important 
+    to be prepared when camping at place, so I made sure to pack a 
     sleeping bag, flashlight, and a """ +noun_1+""". The possibility of seeing a 
     """ +animal_1+""" makes me feel """ +adjective_1+""". I am excited to go 
     hiking on the """ +adjective_2+""" trail. If I see a """ +adjective_3 +""" """ +animal_2+"""
     on the hike, I will take it home as my new pet! The best part of 
-    camping is eating """ +food+""" by the campfire!
+    Camping is eating """ +food+""" by the campfire!
     """)
     
     print("Thanks for playing! Goodbye!")
@@ -41,4 +41,5 @@ if play = "y"
     else:
     print("Goodbye!")
     
+
     
